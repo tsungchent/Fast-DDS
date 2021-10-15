b0e00bf3a21463176c56d131ccfd0916ff12e9bb
Miguel Company <MiguelCompany@eprosima.com>
2021-10-15 11:29:38 +0200

    Refs 12663. Additional PoC expression.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/fastdds/topic/ContentFilteredTopicImpl.hpp
+++ src/cpp/fastdds/topic/ContentFilteredTopicImpl.hpp
@@ -25,16 +25,30 @@
 #include <fastdds/rtps/writer/IReaderDataFilter.hpp>
 
 #include <fastdds/topic/TopicDescriptionImpl.hpp>
+#include <fastdds/topic/TopicImpl.hpp>
 
 namespace eprosima {
 namespace fastdds {
 namespace dds {
 
+namespace poc {
+#include "content-filter-poc/ParameterEvent.h"
+}  // namespace poc
+
 class ContentFilteredTopicImpl : public TopicDescriptionImpl, public eprosima::fastdds::rtps::IReaderDataFilter
 {
 public:
 
-    virtual ~ContentFilteredTopicImpl() = default;
+    virtual ~ContentFilteredTopicImpl()
+    {
+        if (nullptr != data_)
+        {
+            auto related_impl = static_cast<TopicImpl*>(related_topic->get_impl());
+            const TypeSupport& type = related_impl->get_type();
+            type->deleteData(data_);
+            data_ = nullptr;
+        }
+    }
 
     const std::string& get_rtps_topic_name() const override
     {
@@ -47,39 +61,72 @@ public:
     {
         (void)reader_guid;
 
-        // Any expression different from the PoC one will pass the filter
-        if (0 != expression.compare("node = %0") || parameters.empty())
+        // Any expression different from the PoC ones will pass the filter
+        switch (parameters.size())
         {
-            return true;
+            default:
+                return true;
+            case 1:
+                if (0 != expression.compare("node = %0"))
+                {
+                    return true;
+                }
+                break;
+            case 2:
+                if (0 != expression.compare("node = %0 AND changed_parameters[0].name = %1"))
+                {
+                    return true;
+                }
+                break;
         }
 
         // Check minimum length
         uint32_t min_len =
-            change.serializedPayload.representation_header_size +  // RTPS serialized payload header
-            4 + 4 +                                                // stamp
-            4;                                                     // node (string CDR length)
+                change.serializedPayload.representation_header_size + // RTPS serialized payload header
+                4 + 4 +                                               // stamp
+                4;                                                    // node (string CDR length)
         if (min_len >= change.serializedPayload.length)
         {
             // Filtered field not present => filter should pass
             return true;
         }
 
-        eprosima::fastrtps::rtps::CDRMessage_t msg(change.serializedPayload);
-        msg.pos = min_len - 4;
-        std::string data_field;
-
-        if (!eprosima::fastrtps::rtps::CDRMessage::readString(&msg, &data_field))
+        if (parameters.size() == 1)
         {
-            // Malformed string => filter does not pass
-            return false;
+            eprosima::fastrtps::rtps::CDRMessage_t msg(change.serializedPayload);
+            msg.pos = min_len - 4;
+            std::string data_field;
+
+            if (!eprosima::fastrtps::rtps::CDRMessage::readString(&msg, &data_field))
+            {
+                // Malformed string => filter does not pass
+                return false;
+            }
+
+            return 0 == parameters[0].compare(data_field);
         }
 
-        return 0 == parameters[0].compare(data_field);
+        auto related_impl = static_cast<TopicImpl*>(related_topic->get_impl());
+        const TypeSupport& type = related_impl->get_type();
+        if (nullptr == data_)
+        {
+            data_ = (poc::ParameterEvent*)type->createData();
+        }
+        type->deserialize(const_cast<fastrtps::rtps::SerializedPayload_t*>(&change.serializedPayload), data_);
+
+        return
+            data_->node() == parameters[0] &&
+            !data_->changed_parameters().empty() &&
+            data_->changed_parameters()[0].name() == parameters[1];
     }
 
     Topic* related_topic = nullptr;
     std::string expression;
     std::vector<std::string> parameters;
+
+private:
+
+    mutable poc::ParameterEvent* data_ = nullptr;
 };
 
 } /* namespace dds */
